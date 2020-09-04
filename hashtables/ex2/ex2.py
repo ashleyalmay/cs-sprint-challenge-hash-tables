aaa89b953c985fb974b4c4f5115cb1762c824bcf
Ashley <54685244+ashleyalmay@users.noreply.github.com>
2020-09-04 16:02:28 -0400

    mvp

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -4,18 +4,18 @@ class Ticket:
         self.source = source
         self.destination = destination
 
-
-def reconstruct_trip(tickets, length, cache={}):
-
-    # route = [None] * length
-
-    # for ticket in tickets:
-    #     cache[ticket.source] = tickets.destination
-    #     nextdestination = cache['NONE']
-
-    #     for i in range(0,length):
-    #         route[i] = nextdestination
-    #         nextdestination = cache[nextdestination]
-    # return route
-
-    #still looking at this 
+def reconstruct_trip(tickets, length):
+    cache = {}
+    route = []
+#cache and route are both empty
+    for ticket in tickets:
+        cache[ticket.source] = ticket.destination
+    #going though the and making the source = destination pointing to it
+    destination = cache["NONE"] 
+    #set to none  
+    while destination != "NONE":
+        route.append(destination)
+        destination = cache[destination]
+    route.append("NONE")
+    #this is where you add it in
+    return route
